cc1fbf03017da5ed5f424c507f58e60bbb393af7
uni51 <unieye51@gmail.com>
2023-10-18 15:27:48 +0900

    ChangeNotifyProvider および Consumerの使い方

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:counter_provider/count_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,38 +24,43 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    _counter++;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('カウンター'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return ChangeNotifierProvider<CountModel>(
+      create: (_) => CountModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('カウンター'),
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Consumer<CountModel>(builder: (context, model, child) {
+                final count = model.count;
+                return Text(
+                  '$count',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }),
+            ],
+          ),
+        ),
+        floatingActionButton:
+            Consumer<CountModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () {
+              model.incrementCounter();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
+        }),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
