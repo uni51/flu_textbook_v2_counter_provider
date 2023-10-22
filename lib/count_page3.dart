a9e3f5ab53b614b79ac21a984174b7817dbcad1b
uni51 <unieye51@gmail.com>
2023-10-23 06:33:32 +0900

    Counter3を、watchでColorModelを利用する形式に変更して、カウンターの色をランダムに変更

--- lib/count_page3.dart
+++ lib/count_page3.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:counter_provider/count_model.dart';
+import 'package:counter_provider/color_model.dart';
 
 class CountPage3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CountModel>(
-      create: (_) => CountModel(),
+    return ChangeNotifierProvider<ColorModel>(
+      create: (_) => ColorModel(),
       child: Scaffold(
         body: CountPage3Body(),
         floatingActionButton: CountButton3(),
@@ -18,16 +18,17 @@ class CountPage3 extends StatelessWidget {
 class CountPage3Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final model = context.watch<CountModel>();
+    final model = context.watch<ColorModel>();
     final count = model.count;
+    final counterColor = model.counterColor;
     return Center(
       child: Text(
         '$count',
-        style: const TextStyle(
+        style: TextStyle(
           fontSize: 80,
-          color: Colors.red,
+          color: counterColor,
         ),
-      ),
+      )
     );
   }
 }
@@ -35,9 +36,12 @@ class CountPage3Body extends StatelessWidget {
 class CountButton3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final model = context.read<CountModel>();
+    final model = context.read<ColorModel>();
     return FloatingActionButton(
-      onPressed: model.incrementCounter,
+      onPressed: () {
+        model.incrementCounter();
+        model.randomColor();
+      },
       child: const Icon(Icons.add),
     );
   }
